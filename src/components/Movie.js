6495342f4642a2e0b75283569e28b6e0546a8ba2
Elle Townsend <41467324+ellebt@users.noreply.github.com>
2020-10-23 10:03:06 +0100

    created more distinct cards

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,14 +4,18 @@ import React from 'react';
 
 const Movie = (props) => {
     return (
-        <div className="card">
-            {
-                // Minified if statement, condition ? true : false
-                // image = movie.poster
-                props.image == null || props.image === "N/A" ? <div className="default_img">No Image Available</div> : <img src={`https://image.tmdb.org/t/p/w500/${props.image}`} alt="Movie Poster" />
-            }
-            <div className="content">
-                <h4><a href="#">{props.title}</a></h4>
+        <div className="card_wrapper">
+            <div className="card">
+                <a href="#" className="card_link">
+                    {
+                        // Minified if statement, condition ? true : false
+                        // image = movie.poster
+                        props.image == null || props.image === "N/A" ? <div className="default_img">No Image Available</div> : <img src={`https://image.tmdb.org/t/p/w500/${props.image}`} alt="Movie Poster" />
+                    }
+                    <div className="content">
+                        <h4>{props.title}</h4>
+                    </div>
+                </a>
             </div>
         </div>
     )
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -15,9 +15,9 @@ const Pagination = (props) => {
     return (
         <div className="pagination_container">
             <ul className="pagination">
-                {props.currentPage > 1 ? <li className={'pagination_link'} onClick={() => props.nextPage(props.currentPage - 1)}><a href="#">Prev</a></li> : ''}
+                {props.currentPage > 1 ? <li className={'pagination_link'} onClick={() => props.nextPage(props.currentPage - 1)}><a href="#"><i className="fas fa-arrow-left"></i></a></li> : ''}
                 {pageLinks}
-                {props.currentPage < props.pages + 1 ? <li className={'pagination_link'} onClick={() => props.nextPage(props.currentPage + 1)}><a href="#">Next</a></li> : ''}
+                {props.currentPage < props.pages + 1 ? <li className={'pagination_link'} onClick={() => props.nextPage(props.currentPage + 1)}><a href="#"><i className="fas fa-arrow-right"></i></a></li> : ''}
             </ul>
         </div>
     )

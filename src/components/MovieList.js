a274145be6b4465d4b8bc9576054b26e917d721e
Elle Townsend <41467324+ellebt@users.noreply.github.com>
2021-03-10 19:08:34 +0000

    whitespace chnages & favicon update

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,18 +2,23 @@ import React from 'react';
 import Movie from './Movie';
 
 const MovieList = (props) => {
-    return (
-        <section className="movie_list_wrapper">
-            {
-                props.movies.map((movie, i) => { // basically replaces a for loop for rendering each item in the movies array
-                    return (
-                        <Movie key={i} viewMovieInfo={props.viewMovieInfo} image={movie.poster_path} title={movie.title} movieId={movie.id} />
-                        // When rendering a list, need to provide key props
-                    )
-                })
-            }
-        </section>
-    )
-}
+  return (
+    <section className='movie_list_wrapper'>
+      {props.movies.map((movie, i) => {
+        // basically replaces a for loop for rendering each item in the movies array
+        return (
+          <Movie
+            key={i}
+            viewMovieInfo={props.viewMovieInfo}
+            image={movie.poster_path}
+            title={movie.title}
+            movieId={movie.id}
+          />
+          // When rendering a list, need to provide key props
+        );
+      })}
+    </section>
+  );
+};
 
 export default MovieList;
